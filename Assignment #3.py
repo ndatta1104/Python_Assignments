842e514e7222bc4117bd93f1ad9ff4da6685edbb
ndatta1104 <ndatta1104@gmail.com>
2025-01-29 14:25:08 -0500

    Add files via upload

---
+++ Assignment #3.py
@@ -0,0 +1,6 @@
+
+sample_dict = {'a': '100', 'b': '200', 'c': '300'}
+if any 'value' in sample_dict :
+    print("200 is present in the sample_dict.")
+else :
+    print("'20 is not present in the sample_dict.")
